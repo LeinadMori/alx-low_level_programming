973ffa2d7c94f474cff818a7a5566ad01a65ab3b
LeinadMori <iromdaniel@gmail.com>
2023-07-16 17:15:03 -0700

    6-size.c

---
+++ 0x01-variables_if_else_while/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Reture: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("size of the char: %lu byte(s)\n", sizeof(char));
+	printf("size of the int: %lu byte(s)\n", sizeof(int));
+	printf("size of the long int: %lu byte(s)\n", sizeof(long int));
+	printf("size of the long long: %lu byte(s)\n", sizeof(long long));
+	printf("size of the long long: %lu byte(s)\n", sizeof(long long));
+	printf("size of the long long: %lu byte(s)\n", sizeof(float));
+	return (0);
+}
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,22 +5,20 @@
  * Return: Always 0 (success)
  */
 
-int main (void)
-	}
+int main(void)
+{
 	int a;
 
 	for (a = 0; a < 10; a++)
 	{
-		putchar(a + '0');
-		if(a < 9)
+		putchar(a + 0);
+		if (a < 9);
 		}
 			putchar(',');
 			putchar(' ');
-		}
-		
+		{
+
 		}
 		putchar('\n');
-		reture (0);
-		}
-
-
+		return(0);
+}
