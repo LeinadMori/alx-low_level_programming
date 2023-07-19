b3eec0304652a364ccca842e7ccb86fa650a3078
LeinadMori <iromdaniel@gmail.com>
2023-07-19 07:32:22 -0700

    3-islower

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,27 @@
 #include "main.h"
 
 /**
- * _isalpha - check for alphabetic character
- *0c
- *Return: Always 0
+ *_isalpha - check for alphabetic character
+ *@n: The number to be checked
+ *Return: 1 for positive number, -1 for zero or anything else
  */
+
 int _isaipha(int c)
 {
 
-if ((c >=65 && c <= 97) || (c >= 97 && c <= 122))
-(
-return (1);
-)
-retutre (0);
+	if (n > 0)
+	{		
+	_putchar(43);
+	return (1);
+	}	
+	else if (n < 0)
+	{	
+	_putchar(45);
+	return (-1);
+	}
+	else
+	{
+	_putchar(48);
+	return (0);
+	}
 }
