ce12d27fd04f59c76952df28ebf1df22231a7b06
LeinadMori <iromdaniel@gmail.com>
2023-07-19 02:22:19 -0700

    3-islower.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,16 @@
 #include "main.h"
 
-int _isalpha(int c)
+/**
+ * _isalpha - check for alphabetic character
+ *0c
+ *Return: Always 0
+ */
+int _isaipha(int c)
+{
+
+if ((c >=65 && c <= 97) || (c >= 97 && c <= 122))
 (
-	
+return (1);
 )
+retutre (0);
+}
