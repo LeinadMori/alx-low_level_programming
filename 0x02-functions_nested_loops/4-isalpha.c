4629149de0351a578fd32ee7f3270a4731ccc56d
LeinadMori <iromdaniel@gmail.com>
2023-07-20 09:46:10 -0700

    1-main.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,27 +1,12 @@
 #include "main.h"
 
 /**
- *_isalpha - check for alphabetic character
- *@n: The number to be checked
- *Return: 1 for positive number, -1 for zero or anything else
+ *main - check for alphabet
+ *@c: The number to be checked
+ *Return: 1 if c is the letter, 0 otherwise
  */
 
 int _isaipha(int c)
 {
-
-	if (n > 0)
-	{		
-	_putchar(43);
-	return (1);
-	}	
-	else if (n < 0)
-	{	
-	_putchar(45);
-	return (-1);
-	}
-	else
-	{
-	_putchar(48);
-	return (0);
-	}
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
