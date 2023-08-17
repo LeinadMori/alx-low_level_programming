6f0085e0a076e86145e7e48d8a95faadfd1931e7
LeinadMori <iromdaniel@gmail.com>
2023-08-16 17:40:01 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,7 +4,7 @@
  * print_name - print name using pointer to function
  * @name: string to add
  * @f: pointer to function
- * Return: No response 
+ * Reture: nil
  **/
 void print_name(char *name, void (*f)(char *))
 {
